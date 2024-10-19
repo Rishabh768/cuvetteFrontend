6631150aa3b835eb79b869c717da08b3bb337de4
rishabh chauhan <chauhanrishabh768@gmail.com>
2024-10-19 18:44:31 +0530

    logout

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
-import { Link } from 'react-router-dom'
-import {useAuth} from '../component/AuthProvider'
+import React from 'react';
+import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import {useAuth} from '../component/AuthProvider';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+
 const Navbar = () => {
-  const {user}=useAuth();
+  const {user,setUser}=useAuth();
+  const navigate=useNavigate();
+
+  const logout=()=>{
+    Cookies.remove('x-token');
+    setUser(null);
+    navigate('/');
+  }
   return (
     <nav className='flex justify-between p-6  md:px-10 md:py-8'>
        <Link to='/'><img src={logo} alt="cuvette"/> </Link>
@@ -14,9 +24,12 @@ const Navbar = () => {
             <Link to='/signin'>Sign in</Link>
             }
             </li>
+            {user &&
+              <li><button onClick={logout}>Logout</button></li>
+            }
         </ul>
-       
     </nav>
+    
   )
 }
 
